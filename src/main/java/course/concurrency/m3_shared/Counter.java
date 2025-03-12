23e874d3800186f1a323d08245ea64bc9bdced5e
Eugenius <eugene.ekonomov@gmail.com>
2025-03-12 20:38:17 +0500

    [3-2-4] ⭐️ Задание с собеседований: попеременное выполнение

--- src/main/java/course/concurrency/m3_shared/Counter.java
+++ src/main/java/course/concurrency/m3_shared/Counter.java
@@ -1,17 +1,52 @@
 package course.concurrency.m3_shared;
 
+import lombok.SneakyThrows;
+
+import java.util.concurrent.atomic.AtomicInteger;
+
 public class Counter {
+    private static AtomicInteger counter = new AtomicInteger(1);
 
+    @SneakyThrows
     public static void first() {
-
+        synchronized (Counter.class) {
+            while (true) {
+                if (counter.get() == 1) {
+                    System.out.println("1");
+                    counter.set(2);
+                }
+                Counter.class.notifyAll();
+                Counter.class.wait(500);
+            }
+        }
     }
 
+    @SneakyThrows
     public static void second() {
-
+        synchronized (Counter.class) {
+            while (true) {
+                if (counter.get() == 2) {
+                    System.out.println("2");
+                    counter.set(3);
+                }
+                Counter.class.notifyAll();
+                Counter.class.wait(500);
+            }
+        }
     }
 
+    @SneakyThrows
     public static void third() {
-
+        synchronized (Counter.class) {
+            while (true) {
+                if (counter.get() == 3) {
+                    System.out.println("3");
+                    counter.set(1);
+                }
+                Counter.class.notifyAll();
+                Counter.class.wait(500);
+            }
+        }
     }
 
     public static void main(String[] args) {
