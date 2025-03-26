4e5fa42d7860fc941d2755738a7b36fb5f32cd16
Eugenius <eugene.ekonomov@gmail.com>
2025-03-26 18:21:22 +0500

    [3-4-3] ⭐️ Практика: имитация дедлоков В Джава 8 fix

--- src/main/java/course/concurrency/m3_shared/homework/MapDeadlock.java
+++ src/main/java/course/concurrency/m3_shared/homework/MapDeadlock.java
@@ -3,10 +3,29 @@ package course.concurrency.m3_shared.homework;
 import java.util.concurrent.ConcurrentHashMap;
 
 public class MapDeadlock {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
         var map = new ConcurrentHashMap<String, Integer>();
-        var count = map.compute("key", (k, oldValue) -> {
-            return map.putIfAbsent(k, 0) + 1;
-        });
+        Thread t1 = new Thread(() ->
+                map.computeIfAbsent("A", (key) -> {
+                    sleep();
+                    return map.computeIfAbsent("B", key2 -> 1);
+                }));
+        Thread t2 = new Thread(() ->
+                map.computeIfAbsent("B", (key2) -> {
+                    sleep();
+                    return map.computeIfAbsent("A", key -> 1);
+                }));
+        t1.start();
+        t2.start();
+        t1.join();
+        t2.join();
+    }
+
+    private static void sleep() {
+        try {
+            Thread.sleep(1000);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
