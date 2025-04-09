5a3f0c6555b1511a4b4e1f6f3f0df206508b1e17
Eugenius <eugene.ekonomov@gmail.com>
2025-04-09 17:00:23 +0500

    [6-2-3] ⭐️ Практика: экзекьюторы и очереди

--- src/main/java/course/concurrency/m6_streams/ThreadPoolTask.java
+++ src/main/java/course/concurrency/m6_streams/ThreadPoolTask.java
@@ -1,16 +1,27 @@
 package course.concurrency.m6_streams;
 
-import java.util.concurrent.*;
+import java.util.concurrent.LinkedBlockingDeque;
+import java.util.concurrent.SynchronousQueue;
+import java.util.concurrent.ThreadPoolExecutor;
+import java.util.concurrent.TimeUnit;
 
 public class ThreadPoolTask {
 
     // Task #1
     public ThreadPoolExecutor getLifoExecutor() {
-        return null;
+        return new ThreadPoolExecutor(1, Runtime.getRuntime().availableProcessors(), 1L, TimeUnit.MILLISECONDS,
+                new LinkedBlockingDeque() {
+                    @Override
+                    public Object take() throws InterruptedException {
+                        return super.takeLast();
+                    }
+                });
     }
 
     // Task #2
     public ThreadPoolExecutor getRejectExecutor() {
-        return null;
+        return new ThreadPoolExecutor(8, 8, 1, TimeUnit.SECONDS,
+                new SynchronousQueue<>(),
+                new ThreadPoolExecutor.DiscardPolicy());
     }
 }
